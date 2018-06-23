64f5d6533220f34f6bc3e045cec771bf3da2ac5b
Sahil Ajmera <32546795+Sahil-Ajmera@users.noreply.github.com>
2018-06-23 16:45:22 -0400

    Complete First Task - Create Positional Index
    Creating a positional index using tokens extracted directly from the documents.

--- PositionalIndex.java
+++ PositionalIndex.java
@@ -4,6 +4,171 @@ import java.util.ArrayList;
 import java.io.*;
 import java.util.*
 
+
+public class PositionalIndex {
+    String[] myDocs;
+    ArrayList<String> termList;
+    ArrayList<ArrayList<DocId>> docLists;
+
+    /**
+     * Construct a positional index
+     * @param docs List of input strings or file names
+     *
+     */
+    public PositionalIndex(String[] docs)
+    {
+        //TASK1: TO BE COMPLETED
+        System.out.println("****************************************Task-1****************************************");
+        // Taking all string into one.
+
+        termList = new ArrayList<String>();
+        docLists = new ArrayList<ArrayList<DocId>>();
+
+        try
+        {
+
+            for(int i = 0 ; i< docs.length;i++)
+            {
+                String all_lines = "";
+                File docFile = new File("Lab1_Data" + '/' + docs[i]);
+                BufferedReader reader = new BufferedReader(new FileReader(docFile));
+                String line = null;
+                while((line = reader.readLine()) != null)
+                {
+
+                    // Normalization - Take all lower case  .
+                    all_lines +=  line.toLowerCase();
+                }
+
+                // Tokenization
+                String[] tokens  = all_lines.split("[ .,?!:;$%&*+()%#!/\\-\\^\"]+");
+
+                for(int j =0 ; j<tokens.length;j++)
+                {
+                    if(!termList.contains(tokens[j]))
+                    {
+                        termList.add(tokens[j]);
+                        DocId doid = new DocId(i,j);
+                        ArrayList<DocId> docList = new ArrayList<DocId>();
+                        docList.add(doid);
+                        docLists.add(docList);
+                    }
+                    else
+                    {
+                        int index = termList.indexOf(tokens[j]);
+                        ArrayList<DocId> docList = docLists.get(index);
+                        boolean match = false;
+                        int k = 0;
+
+                        //old term same document also seen before
+                        for(DocId doid:docList)
+                        {
+                            if(doid.docId == i)
+                            {
+                                doid.insertPosition(j);
+                                match = true;
+                                //docList.set(k,doid);
+                            }
+                            k++;
+                        }
+
+                        //old term new document
+                        if(!match)
+                        {
+                            DocId doid = new DocId(i,j);
+                            docLists.get(index).add(doid);
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            for(int i = 0 ;i<docs.length;i++)
+            {
+                String all_lines = "";
+                all_lines += docs[i].toLowerCase();
+
+                // Tokenization
+                String[] tokens  = all_lines.split("[ .,?!:;$%&*+()%#!/\\-\\^\"]+");
+
+                for(int j =0 ; j<tokens.length;j++)
+                {
+                    if(!termList.contains(tokens[j]))
+                    {
+                        termList.add(tokens[j]);
+                        DocId doid = new DocId(i,j);
+                        ArrayList<DocId> docList = new ArrayList<DocId>();
+                        docList.add(doid);
+                        docLists.add(docList);
+                    }
+                    else
+                    {
+                        int index = termList.indexOf(tokens[j]);
+                        ArrayList<DocId> docList = docLists.get(index);
+                        boolean match = false;
+                        int k = 0;
+
+                        //old term same document also seen before
+                        for(DocId doid:docList)
+                        {
+                            if(doid.docId == i)
+                            {
+                                doid.insertPosition(j);
+                                match = true;
+                                //docList.set(k,doid);
+                            }
+                            k++;
+                        }
+
+                        //old term new document
+                        if(!match)
+                        {
+                            DocId doid = new DocId(i,j);
+                            docLists.get(index).add(doid);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Return the string representation of a positional index
+     */
+    public String toString()
+    {
+        String matrixString = new String();
+        ArrayList<DocId> docList;
+        for(int i=0;i<termList.size();i++){
+            matrixString += String.format("%-15s", termList.get(i));
+            docList = docLists.get(i);
+            for(int j=0;j<docList.size();j++)
+            {
+                matrixString += docList.get(j)+ "\t";
+            }
+            matrixString += "\n";
+        }
+        return matrixString;
+    }
+
+
+
+   public static void main(String[] args)
+    {
+        String[] docs = {"new home sales top forecasts",
+                "home sales rise in july",
+                "increase in home sales in july",
+                "july new home sales rise"
+        };
+        Scanner scanner = new Scanner(System.in);
+        //place files in Lab1_Data folder if using files
+        //String[] docs = {"cv000_29416.txt","cv001_19502.txt","cv002_17424.txt","cv003_12683.txt","cv004_12641.txt"};
+        PositionalIndex pi = new PositionalIndex(docs);
+        System.out.print(pi);
+    }
+
 //Supporting Class
 /**
  *
